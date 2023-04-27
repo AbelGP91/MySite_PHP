ee9d279a4ae7d528085b7135036a63a72d26cc93
Abel Galindo <abel.gp.1991@hotmail.es>
2023-04-27 09:26:06 +0200

    Cambios Ejercicio 3

--- Tema_2/Exercici_3.php
+++ Tema_2/Exercici_3.php
@@ -5,7 +5,20 @@
 
 echo "<pre>";
 
+$x = 3;
+$y = 5;
 
+$n = 3.5;
+$m = 7.2;
+
+var_dump($x);
+var_dump($y);
+var_dump($n);
+var_dump($m);
+
+echo "<br>";
+
+echo "$x + $y = " . ($x+$y);
 
 
 echo "</pre>";
